77c79dacb20b54041afb184f46568414abaa01f3
HashemJalloul <jalloulhashem@gmail.com>
2025-05-05 19:50:08 +0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
 
   return (
     <>
+      <h1>hello</h1>
       <Nav logo="Vaccination.ng" items={[
         { link: "#", content: "Home" }, { link: "#", content: "Services" },
         { link: "#", content: "Schedule" }, { link: "#", content: "FeedBack" }
